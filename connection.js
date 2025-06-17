b45163258d61baaaa24c74434191bd25bd80fa9c
parasramgaikwad <138496629+parasramgaikwad@users.noreply.github.com>
2025-06-17 10:21:26 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -2,11 +2,18 @@ var mysql = require("mysql");
 var util = require("util");
 
 var conn = mysql.createConnection({
-    "host": "localhost",
-    "user": "root",
-    "password": "",
-    "database": "janlakshmi_miltistate"
-})
+    "host": "bvytvgqvpaldk9kr7m32-mysql.services.clever-cloud.com",
+    "user": "usr3j0ec6k4syzcl",
+    "password": "FgUMmMvNBhfPHhr1srbz",
+    "database": "bvytvgqvpaldk9kr7m32"
+});
+
+// var conn = mysql.createConnection({
+//     "host": "localhost",
+//     "user": "root",
+//     "password": "",
+//     "database": "janlakshmi_miltistate"
+// })
 
 var exe = util.promisify(conn.query).bind(conn);
 
