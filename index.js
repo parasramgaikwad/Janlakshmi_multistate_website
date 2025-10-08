3224c36466f2a6243cd2573cb08179534a700fde
parasramgaikwad <138496629+parasramgaikwad@users.noreply.github.com>
2025-10-08 22:11:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ var upload = require("express-fileupload");
 app.use(upload());
 app.use(express.static("public/"));
 var session = require("express-session");
-
 app.use(session({
     secret: "1234554321",
     saveUninitialized: true,
     resave: true
 }));
-
 var user_route = require("./routes/user_route");
 var admin_route = require("./routes/admin_route");
 
